7b776bcaf188987ac8c259df77781d9dd492ac88
User-49 <kapil.negi12300@gmail.com>
2024-12-15 20:59:30 +0530

    upt

--- Project 3/sql data installer.py
+++
@@ -1,7 +0,0 @@
-import mysql.connector
-sqldb = mysql.connector.connect(user='root',passwd='kapil2006@_MYSQL',host='localhost')
-csr = sqldb.cursor()
-f = open('D:\projects\Projects--repository\Project 3\mysql_data.txt','r',encoding='UTF-8')
-for i in f.readlines():
-    csr.execute(i[:-1])
-sqldb.commit()
